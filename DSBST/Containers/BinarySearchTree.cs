af927a59a2c3b07aa5fbfe2ff6188d03c125235f
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-03-29 15:30:25 +0800

    Add initial BST node implementation

---
+++ DSBST/Containers/BinarySearchTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DSBST.Containers
+{
+    public class BinarySearchTreeNode : IComparable<BinarySearchTreeNode>
+    {
+        public int Value;
+        public BinarySearchTreeNode Parent;
+        public BinarySearchTreeNode Left;   // LL's prev
+        public BinarySearchTreeNode Right;  // LL's next
+
+        public BinarySearchTreeNode(int val)
+        {
+            Value = val;
+        }
+
+        public bool HasParent => Parent != null;
+
+        public bool HasLeft => Left != null;
+
+        public bool HasRight => Right != null;
+
+        public bool IsLeftChild => Parent.Left != null && Parent.Left == this;
+
+        public bool IsRightChild => Parent.Right != null && Parent.Right == this;
+
+        public bool IsLeaf => !HasRight && !HasLeft;
+
+        public int CompareTo(BinarySearchTreeNode other)
+        {
+            if (other == null)
+                return 1;
+            return Value.CompareTo(other.Value);
+        }
+    }
+
+    class BinarySearchTree
+    {
+    }
+}
