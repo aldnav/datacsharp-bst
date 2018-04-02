3ddba0d6e87e27a711d24f7ce35b84bbfc4fdbae
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-04-02 22:00:21 +0800

    Update test to include traversals

--- DSBST/Program.cs
+++ DSBST/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DSBST.Containers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace DSBST
     {
         static void Main(string[] args)
         {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+
+            tree.Insert(5);
+            tree.Insert(2);
+            tree.Insert(7);
+            tree.Insert(1);
+            tree.Insert(9);
+
+            System.Console.WriteLine("Pre order");
+            foreach(BinarySearchTreeNode<int> node in tree.Traverse(Traversal.PreOrder))
+            {
+                System.Console.WriteLine(node.Value);
+            }
+
+            System.Console.WriteLine("In order");
+            foreach (BinarySearchTreeNode<int> node in tree.Traverse())
+            {
+                System.Console.WriteLine(node.Value);
+            }
+
+            System.Console.WriteLine("Post order");
+            foreach (BinarySearchTreeNode<int> node in tree.Traverse(Traversal.PostOrder))
+            {
+                System.Console.WriteLine(node.Value);
+            }
+
+            System.Console.Read();
         }
     }
 }
